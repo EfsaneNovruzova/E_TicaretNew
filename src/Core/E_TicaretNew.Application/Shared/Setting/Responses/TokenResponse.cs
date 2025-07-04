297d93e5cd0cae3ae49e6a680206be4561b59973
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-07-05 00:38:51 +0400

    Create RefreshToken

--- src/Core/E_TicaretNew.Application/Shared/Setting/Responses/TokenResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace E_TicaretNew.Application.Shared.Setting.Responses;
-
-public  class TokenResponse
-{
-    public string? Token { get; set; }
-    public string? RefreshToken{ get; set; }
-    public DateTime? ExpireDate { get; set; }
-}
