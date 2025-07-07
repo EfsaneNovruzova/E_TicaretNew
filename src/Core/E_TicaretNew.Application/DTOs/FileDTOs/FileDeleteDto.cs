bb4ba5b3495533b46ae7554633718f1bdbaff42e
EfsaneNovruzova <novruzovafsan88@gmail.com>
2025-07-07 20:37:39 +0400

    Create File

---
+++ src/Core/E_TicaretNew.Application/DTOs/FileDTOs/FileDeleteDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_TicaretNew.Application.DTOs.FileDTOs;
+
+public class FileDeleteDto
+{
+    [Required]
+    public string FilePath { get; set; }
+}
---
+++ src/Presentation/E_TicaretNew.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,33 @@
+﻿using E_TicaretNew.Application.DTOs.FileDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static E_TicaretNew.Application.Abstracts.Services.IFileService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FilesController : ControllerBase
+{
+    private readonly IFileService _fileService;
+
+    public FilesController(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    [HttpPost("upload")]
+    [Authorize(Policy = "Files.Upload")]
+    public async Task<IActionResult> Upload([FromForm] FileUploadDto dto)
+    {
+        var result = await _fileService.UploadFileAsync(dto);
+        return StatusCode((int)result.StatusCode, result);
+    }
+
+    [HttpDelete("delete")]
+    [Authorize(Policy = "Files.Delete")]
+    public async Task<IActionResult> Delete([FromQuery] FileDeleteDto dto)
+    {
+        var result = await _fileService.DeleteFileAsync(dto);
+        return StatusCode((int)result.StatusCode, result);
+    }
+}
+
